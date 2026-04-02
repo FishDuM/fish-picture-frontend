a8f5ff34a4da1cbe0e2eba943d9e91be50efffaa
会飞的鱼 <3057433102@qq.com>
2026-04-02 18:22:05 +0800

    管理员批量抓图

--- src/api/typings.d.ts
+++ src/api/typings.d.ts
@@ -5,6 +5,12 @@ declare namespace API {
     message?: string
   }
 
+  type BaseResponseInt_ = {
+    code?: number
+    data?: number
+    message?: string
+  }
+
   type BaseResponseLoginUserVO_ = {
     code?: number
     data?: LoginUserVO
@@ -203,6 +209,12 @@ declare namespace API {
     tags?: string[]
   }
 
+  type PictureUploadByBatchRequest = {
+    count?: number
+    namePrefix?: string
+    searchText?: string
+  }
+
   type PictureVO = {
     category?: string
     createTime?: string
@@ -230,11 +242,13 @@ declare namespace API {
   type uploadPictureByUrlUsingPOSTParams = {
     fileUrl?: string
     id?: number
+    picName?: string
   }
 
   type uploadPictureUsingPOSTParams = {
     fileUrl?: string
     id?: number
+    picName?: string
   }
 
   type User = {
--- src/pages/admin/PictureManagePage.vue
+++ src/pages/admin/PictureManagePage.vue
@@ -1,111 +1,146 @@
 <template>
   <div class="pictureManage">
-    <!--    搜索框-->
-    <a-form layout="inline" :model="searchParams" @finish="doSearch">
-      <a-form-item label="关键词">
-        <a-input
-          v-model:value="searchParams.searchText"
-          placeholder="从名称和简介搜索"
-          allow-clear
-        />
-      </a-form-item>
-      <a-form-item label="类型">
-        <a-input v-model:value="searchParams.category" placeholder="请输入图片类型" allow-clear />
-      </a-form-item>
-      <a-form-item label="标签">
-        <a-select
-          v-model:value="searchParams.tags"
-          mode="tags"
-          style="min-width: 180px"
-          placeholder="请输入标签"
-          allow-clear
-        />
-      </a-form-item>
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="图片管理">
+        <!--    搜索框-->
+        <a-form layout="inline" :model="searchParams" @finish="doSearch">
+          <a-form-item label="关键词">
+            <a-input
+              v-model:value="searchParams.searchText"
+              placeholder="从名称和简介搜索"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="类型">
+            <a-input
+              v-model:value="searchParams.category"
+              placeholder="请输入图片类型"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="标签">
+            <a-select
+              v-model:value="searchParams.tags"
+              mode="tags"
+              style="min-width: 180px"
+              placeholder="请输入标签"
+              allow-clear
+            />
+          </a-form-item>
 
-      <a-form-item label="审核状态">
-        <a-input-group compact>
-          <a-select v-model:value="searchParams.reviewstatus">
-            <a-select-option :value="PIC_REVIEW_STATUS_ENUM.REVIEWING">待审核</a-select-option>
-            <a-select-option :value="PIC_REVIEW_STATUS_ENUM.PASS">已通过</a-select-option>
-            <a-select-option :value="PIC_REVIEW_STATUS_ENUM.REJECT">已拒绝</a-select-option>
-          </a-select>
-        </a-input-group>
-      </a-form-item>
+          <a-form-item label="审核状态">
+            <a-input-group compact>
+              <a-select v-model:value="searchParams.reviewstatus">
+                <a-select-option :value="PIC_REVIEW_STATUS_ENUM.REVIEWING">待审核</a-select-option>
+                <a-select-option :value="PIC_REVIEW_STATUS_ENUM.PASS">已通过</a-select-option>
+                <a-select-option :value="PIC_REVIEW_STATUS_ENUM.REJECT">已拒绝</a-select-option>
+              </a-select>
+            </a-input-group>
+          </a-form-item>
 
-      <a-form-item>
-        <a-button type="primary" html-type="submit">搜索</a-button>
-      </a-form-item>
-      <div style="margin-bottom: 20px"></div>
-    </a-form>
+          <a-form-item>
+            <a-button type="primary" html-type="submit">搜索</a-button>
+          </a-form-item>
+          <div style="margin-bottom: 20px"></div>
+        </a-form>
 
-    <!--    表格-->
-    <a-table
-      :columns="columns"
-      :data-source="pictureList"
-      :pagination="pagination"
-      @change="doTableChange"
-      :scroll="{ x: 'max-content' }"
-    >
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'url'">
-          <a-image style="width: 50px" :src="record.url" />
-        </template>
+        <!--    表格-->
+        <a-table
+          :columns="columns"
+          :data-source="pictureList"
+          :pagination="pagination"
+          @change="doTableChange"
+          :scroll="{ x: 'max-content' }"
+        >
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.dataIndex === 'url'">
+              <a-image style="width: 50px" :src="record.url" />
+            </template>
 
-        <template v-if="column.dataIndex === 'tags'">
-          <a-space wrap>
-            <a-tag v-for="tag in JSON.parse(record.tags || '[]')" :key="tag"> {{ tag }}</a-tag>
-          </a-space>
-        </template>
+            <template v-if="column.dataIndex === 'tags'">
+              <a-space wrap>
+                <a-tag v-for="tag in JSON.parse(record.tags || '[]')" :key="tag"> {{ tag }}</a-tag>
+              </a-space>
+            </template>
 
-        <template v-if="column.dataIndex === 'picInfo'">
-          <div>格式：{{ record.picFormat }}</div>
-          <div>宽度：{{ record.picWidth }}</div>
-          <div>高度：{{ record.picHeight }}</div>
-          <div>宽高比：{{ record.picWidth / record.picHeight }}</div>
-          <div>大小：{{ (record.picSize / 1024).toFixed(2) + 'KB' }}</div>
-        </template>
+            <template v-if="column.dataIndex === 'picInfo'">
+              <div>格式：{{ record.picFormat }}</div>
+              <div>宽度：{{ record.picWidth }}</div>
+              <div>高度：{{ record.picHeight }}</div>
+              <div>宽高比：{{ record.picWidth / record.picHeight }}</div>
+              <div>大小：{{ (record.picSize / 1024).toFixed(2) + 'KB' }}</div>
+            </template>
 
-        <template v-if="column.dataIndex === 'reviewMessage'">
-          <div>审核状态：{{ PIC_REVIEW_STATUS_MAP[record.reviewstatus] }}</div>
-          <div>审核信息：{{ record.reviewmessage }}</div>
-          <div>审核人：{{ record.reviewerid }}</div>
-          <div v-if="record.reviewtime">
-            审核时间：{{ dayjs(record.reviewtime).format('YYYY-MM-DD HH:mm:ss') }}
-          </div>
-        </template>
+            <template v-if="column.dataIndex === 'reviewMessage'">
+              <div>审核状态：{{ PIC_REVIEW_STATUS_MAP[record.reviewstatus] }}</div>
+              <div>审核信息：{{ record.reviewmessage }}</div>
+              <div>审核人：{{ record.reviewerid }}</div>
+              <div v-if="record.reviewtime">
+                审核时间：{{ dayjs(record.reviewtime).format('YYYY-MM-DD HH:mm:ss') }}
+              </div>
+            </template>
 
-        <template v-if="column.dataIndex === 'createTime'">
-          <div>{{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}</div>
-        </template>
+            <template v-if="column.dataIndex === 'createTime'">
+              <div>{{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}</div>
+            </template>
 
-        <template v-if="column.dataIndex === 'editTime'">
-          <div>{{ dayjs(record.editTime).format('YYYY-MM-DD HH:mm:ss') }}</div>
-        </template>
+            <template v-if="column.dataIndex === 'editTime'">
+              <div>{{ dayjs(record.editTime).format('YYYY-MM-DD HH:mm:ss') }}</div>
+            </template>
 
-        <template v-else-if="column.key === 'action'">
-          <a-space wrap>
-            <a-button
-              v-if="record.reviewstatus !== PIC_REVIEW_STATUS_ENUM.PASS"
-              type="link"
-              @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)"
-              >通过</a-button
+            <template v-else-if="column.key === 'action'">
+              <a-space wrap style="max-width: 40px">
+                <a-button
+                  v-if="record.reviewstatus !== PIC_REVIEW_STATUS_ENUM.PASS"
+                  type="link"
+                  @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)"
+                  >通过</a-button
+                >
+                <a-button
+                  type="link"
+                  danger
+                  v-if="record.reviewstatus !== PIC_REVIEW_STATUS_ENUM.REJECT"
+                  @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT)"
+                  target="_blank"
+                  >拒绝</a-button
+                >
+                <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank"
+                  >编辑</a-button
+                >
+                <a-button type="primary" @click="doDelete(record.id)" danger ghost>删除</a-button>
+              </a-space>
+            </template>
+          </template>
+        </a-table>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="批量创建图片" force-render>
+        <a-form name="basic" autocomplete="off" style="max-width: 50%; margin: 30px auto">
+          <a-form-item label="数量" name="count">
+            <a-input-number
+              :min="1"
+              :max="30"
+              v-model:value="searchAllUrl.count"
+              placeholder="请输入获取图片的数量"
+              style="width: 100%"
+            />
+          </a-form-item>
+
+          <a-form-item label="图片命名前缀" name="namePrefix">
+            <a-input v-model:value="searchAllUrl.namePrefix" placeholder="请输入图片名称前缀" />
+          </a-form-item>
+
+          <a-form-item label="图片搜索关键词" name="searchText">
+            <a-input v-model:value="searchAllUrl.searchText" placeholder="请输入图片搜索关键词" />
+          </a-form-item>
+
+          <a-form-item>
+            <a-button style="width: 100%" type="primary" @click="doSearchAllByUrl"
+              >批量抓取</a-button
             >
-            <a-button
-              type="link"
-              danger
-              v-if="record.reviewstatus !== PIC_REVIEW_STATUS_ENUM.REJECT"
-              @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT)"
-              target="_blank"
-              >拒绝</a-button
-            >
-            <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank"
-              >编辑</a-button
-            >
-            <a-button type="primary" @click="doDelete(record.id)" danger ghost>删除</a-button>
-          </a-space>
-        </template>
-      </template>
-    </a-table>
+          </a-form-item>
+        </a-form>
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
@@ -115,6 +150,7 @@ import {
   deletePictureUsingPost,
   doPictureReviewUsingPost,
   listPictureByPageUsingPost,
+  uploadPictureByBatchUsingPost,
 } from '@/api/pictureController.ts'
 import dayjs from 'dayjs'
 import { message } from 'ant-design-vue'
@@ -250,6 +286,23 @@ const handleReview = async (record: API.Picture, status: number) => {
     await searchParam()
   } else {
     message.error('审核失败' + res.data.message)
+  }
+}
+
+const activeKey = ref('1')
+
+const searchAllUrl = ref<API.PictureUploadByBatchRequest>({
+  count: 1,
+  namePrefix: '',
+  searchText: '',
+})
+
+const doSearchAllByUrl = async () => {
+  const res = await uploadPictureByBatchUsingPost(searchAllUrl.value)
+  if (res.data.code === 0) {
+    message.success('批量获取成功')
+  } else {
+    message.error('批量获取失败' + res.data.message)
   }
 }
 </script>
